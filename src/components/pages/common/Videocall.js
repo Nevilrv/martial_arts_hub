085474beb34eb892342dacda189c00f6f545c6d7
Nevilrv <nevilrv@gmail.com>
2024-11-27 17:07:42 +0530

    fixed UI issue in video call 3

--- src/components/pages/common/Videocall.js
+++ src/components/pages/common/Videocall.js
@@ -13,50 +13,48 @@ const Videocall = () => {
   const [videoCall, setVideoCall] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const initAgora = async () => {
-      try {
-        // Fetch the token from your server
-        const response = await axios.post(
-          `${baseURL}/meeting/genrateToken`,
-          {
-            channelName,
-            role,
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`, // Replace with your actual token
-            },
-          }
-        );
-        const token = response.data.data;
-
-        // Set RTC properties for Agora UIKit
-        setRtcProps({
-          appId: "fe27d8a87b6d4614bb016f58bf0e286a",
-          channel: channelName,
-          token,
+  const initAgora = async () => {
+    try {
+      const response = await axios.post(
+        `${baseURL}/meeting/genrateToken`,
+        {
+          channelName,
           role,
-          enableVideo: true,
-          enableAudio: true,
-        });
-        toast.success(response.message);
-      } catch (error) {
-        if (error === "Please log in to access this resource") {
-          const userRole = JSON.parse(localStorage.getItem("Role"));
-          if (userRole === "Student") {
-            navigate(Routing.StudentLogin);
-          } else if (userRole === "Instructor") {
-            navigate(Routing.InstructorLogin);
-          } else {
-            console.error("Failed to join room:", error);
-          }
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`, 
+          },
+        }
+      );
+      const token = response.data.data;
+      setRtcProps({
+        appId: "fe27d8a87b6d4614bb016f58bf0e286a",
+        channel: channelName,
+        token,
+        role,
+        enableVideo: true,
+        enableAudio: true,
+      });
+      toast.success(response.message);
+    } catch (error) {
+      if (error === "Please log in to access this resource") {
+        const userRole = JSON.parse(localStorage.getItem("Role"));
+        if (userRole === "Student") {
+          navigate(Routing.StudentLogin);
+        } else if (userRole === "Instructor") {
+          navigate(Routing.InstructorLogin);
+        } else {
+          console.error("Failed to join room:", error);
         }
       }
-    };
+    }
+  };
+
+  useEffect(() => {
     initAgora();
     // eslint-disable-next-line
-  }, [channelName, role, videoCall]);
+  }, [channelName, role]);
   const callbacks = {
     EndCall: () => setVideoCall(false),
   };
@@ -72,11 +70,16 @@ const Videocall = () => {
       <OutlineBtn
         text={"Start Call"}
         className={"bg-Green-100 border-green text-green"}
-        onClick={() => setVideoCall(true)}
+        onClick={() => {
+          setVideoCall(true);
+          initAgora();
+        }}
       />
     </div>
   ) : (
-    <div style={{ display: "flex", width: "100%", height: "calc(100vh - 85px)" }}>
+    <div
+      style={{ display: "flex", width: "100%", height: "calc(100vh - 85px)" }}
+    >
       {rtcProps ? (
         <AgoraUIKit
           rtcProps={rtcProps}
