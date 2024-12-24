0111b15263e09e2d5ff6d24056a4e74bfee20d90
Nevilrv <nevilrv@gmail.com>
2024-12-24 12:43:24 +0530

    Instructor Login

--- src/components/pages/Instructor/Signup/Index.js
+++ src/components/pages/Instructor/Signup/Index.js
@@ -43,7 +43,7 @@ const Index = () => {
       localStorage.setItem("_id", JSON.stringify(result?.data?.instructorId));
       localStorage.setItem("token", JSON.stringify(result?.Token));
       localStorage.setItem("is_login", true);
-      navigate(Routing.InstructorProfile);
+      navigate(Routing.InstructorLogin);
     } else {
       setLoading(false);
       toast.error(result?.message);
