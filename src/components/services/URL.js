21a645b3d925b88132eaa8cd9493fc6a02f3dea0
Yash9510 <yashvekariya9510@gmail.com>
2025-01-09 14:12:56 +0530

    Chnage urls

--- src/components/services/URL.js
+++ src/components/services/URL.js
@@ -1,4 +1,4 @@
 // export const baseURL = `https://martial-arts-hub-backend.onrender.com/api`;
-//export const baseURL = `https://martialartshub.co/api`;
-export const baseURL = `http://localhost:4000/api`;
+export const baseURL = `https://martialartshub.co/api`;
+//export const baseURL = `http://localhost:4000/api`;
 
