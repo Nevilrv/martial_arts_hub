3611baf18fd3605d8af977b0dec68948b577e40a
Nevil Vaghasiya <43700430+Nevilrv@users.noreply.github.com>
2025-03-21 11:38:53 +0530

    fix modeule isshue

--- src/components/pages/common/ProfileAdujust/StudentProfileAdjust.js
+++ src/components/pages/common/ProfileAdujust/StudentProfileAdjust.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import Cropper from 'react-easy-crop'
+import React, { useState } from "react";
+import Cropper from "react-easy-crop";
+import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Dialog from "@mui/material/Dialog";
@@ -7,22 +8,79 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slider from "@mui/material/Slider";
-import { styled } from "@mui/material/styles";
 
-import { styles } from './styles'
+// Styled components
+const CropContainer = styled("div")(({ theme }) => ({
+  position: "relative",
+  width: "100%",
+  height: 200,
+  background: "#D6D3CA",
+  [theme.breakpoints.up("sm")]: {
+    height: 400,
+  },
+}));
 
-const Demo = ({ classes, open, onClose, image, crop, rotation, zoom, onCropChange, onRotationChange, onCropComplete, onZoomChange, setZoomchange, setRoationchnage, onClick, showCroppedImageOnclick, children }) => {
+const Controls = styled("div")(({ theme }) => ({
+  padding: 16,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "stretch",
+  [theme.breakpoints.up("sm")]: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+}));
 
+const SliderContainer = styled("div")({
+  display: "flex",
+  flex: "1",
+  alignItems: "center",
+});
 
+const SliderLabel = styled(Typography)(({ theme }) => ({
+  fontWeight: "bold",
+  [theme.breakpoints.down("xs")]: {
+    minWidth: 65,
+  },
+}));
+
+const StyledSlider = styled(Slider)(({ theme }) => ({
+  padding: "22px 0px",
+  marginLeft: 16,
+  color: "#000",
+  [theme.breakpoints.up("sm")]: {
+    flexDirection: "row",
+    alignItems: "center",
+    margin: "0 16px",
+  },
+}));
+
+const Demo = ({
+  open,
+  onClose,
+  image,
+  crop,
+  rotation,
+  zoom,
+  onCropChange,
+  onRotationChange,
+  onCropComplete,
+  onZoomChange,
+  setZoomchange,
+  setRoationchnage,
+  onClick,
+  showCroppedImageOnclick,
+  children,
+}) => {
   return (
     <div>
       {children}
 
       {/* Dialog for Image Cropping */}
       <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
-        <DialogTitle className='bg-primary_dark'>Crop Image</DialogTitle>
-        <DialogContent className='bg-primary_dark'>
-          <div className={classes.cropContainer}>
+        <DialogTitle className="bg-primary_dark">Crop Image</DialogTitle>
+        <DialogContent className="bg-primary_dark">
+          <CropContainer>
             <Cropper
               image={image}
               crop={crop}
@@ -34,53 +92,46 @@ const Demo = ({ classes, open, onClose, image, crop, rotation, zoom, onCropChang
               onCropComplete={onCropComplete}
               onZoomChange={onZoomChange}
             />
-          </div>
-          <div className={classes.controls}>
-            <div className={classes.sliderContainer}>
-              <Typography variant="overline" classes={{ root: classes.sliderLabel }}>
-                Zoom
-              </Typography>
-              <Slider
+          </CropContainer>
+          <Controls>
+            <SliderContainer>
+              <SliderLabel variant="overline">Zoom</SliderLabel>
+              <StyledSlider
                 value={zoom}
                 min={1}
                 max={3}
                 step={0.1}
                 aria-labelledby="Zoom"
-                classes={{ root: classes.slider }}
                 onChange={setZoomchange}
               />
-            </div>
-            <div className={classes.sliderContainer}>
-              <Typography variant="overline" classes={{ root: classes.sliderLabel }}>
-                Rotation
-              </Typography>
-              <Slider
+            </SliderContainer>
+            <SliderContainer>
+              <SliderLabel variant="overline">Rotation</SliderLabel>
+              <StyledSlider
                 value={rotation}
                 min={0}
                 max={360}
                 step={1}
-                color=''
                 aria-labelledby="Rotation"
-                classes={{ root: classes.slider }}
                 onChange={setRoationchnage}
               />
-            </div>
-          </div>
+            </SliderContainer>
+          </Controls>
         </DialogContent>
-        <DialogActions className='bg-primary_dark'>
+        <DialogActions className="bg-primary_dark">
           <Button onClick={onClick} color="dark">
             Cancel
           </Button>
-          <button onClick={showCroppedImageOnclick} className='bg-Dark_black text-white px-3 py-1 rounded-md'>
+          <button
+            onClick={showCroppedImageOnclick}
+            className="bg-Dark_black text-white px-3 py-1 rounded-md"
+          >
             Crop & Save
           </button>
         </DialogActions>
       </Dialog>
     </div>
-  )
-}
+  );
+};
 
-
-const StyledDemo = styled(styles)(Demo)
-
-export default StyledDemo
+export default Demo;
